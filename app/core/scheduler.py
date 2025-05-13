47d4b5758bef74b36af4a22d537b98985bbfe7f2
sufiyakhalid <khalidsiddiqui9550@gmail.com>
2025-05-13 23:07:58 +0530

    retriggered

--- app/core/scheduler.py
+++ app/core/scheduler.py
@@ -22,7 +22,7 @@ async def schedule_async_task(task_func, *args):
 
 def setup_scheduled_tasks(scheduler):
     fetch_stock_trigger = CronTrigger(
-        second=0, minute=2, hour=23, day="*", month="*", day_of_week="0-6", timezone=ASIA_KOLKATA
+        second=0, minute=15, hour=23, day="*", month="*", day_of_week="0-6", timezone=ASIA_KOLKATA
     )
     buy_trigger = CronTrigger(
         second=5, minute=16, hour=15, day="*", month="*", day_of_week="0-4", timezone=ASIA_KOLKATA
